347c238f5d4f241f665ada632957eed0d2d9241b
Lenovo <bogdannerushev@gmail.com>
2024-04-19 21:52:50 +0200

    erste_comit3

--- main2.py
+++ main2.py
@@ -3,11 +3,13 @@ from datetime import datetime
 
 def get_days_from_today(date:str) -> int:
     try:
-        return (datetime.strptime(date, "%Y-%m-%d")-datetime.now()).days
+        return (datetime.strptime(date, "%Y-%m-%d").date() - datetime.now().date()).days
     except ValueError:
         return ('Некоректний формат вводу !!!')
 a = input("Введіть дату: ")
 print(get_days_from_today(a))
+
+
 
 #   =======№2=======
 from random import randint
@@ -29,30 +31,34 @@ def get_numbers_ticket(minim = 1, maximum = 1000, quantity = 1):
     else:
         return zahlen_list
 
-min_zahlen, max_zahlen, count_zahlen =  int(input()), int(input()), int(input())
+min_zahlen, max_zahlen, count_zahlen =  int(input('Введіть мінімальне число: ')), int(input('Введіть максимальне число: ')), int(input('Введіть кількість чисел у вибірці: '))
 print("Ваші лотерейні числа:", get_numbers_ticket(min_zahlen, max_zahlen, count_zahlen))
 
 #   =======№3=======
 import re
 
+
 def normalize_phone(phone_number):
     formatted_phone = []
-    for i in range(len(phone_number)):
-        pattern = r"[;,\-:!\.()+\\t n]"
-        replacement = r""
-        formatted_phone.append('+' + '38' + re.sub(pattern, replacement, phone_number[i])[-10:])
+
+    pattern = r"[;,\-:!\.()+\\t n]"
+    replacement = r""
+    formatted_phone.append('+' + '38' + re.sub(pattern, replacement, phone_number)[-10:])
     return formatted_phone
 
-phone = [
-    "067\\t123 4567",
-    "(095) 234-5678\\n",
-    "+380 44 123 4567",
-    "380501234567",
-    "    +38(050)123-32-34",
-    "     0503451234",
-    "(050)8889900",
-    "38050-111-22-22",
-    "38050 111 22 11   ",
-]
+
+phone = input('Введіть номер для корекції: ')
 
 print(normalize_phone(phone))
+
+#phone = [
+#    "067\\t123 4567",
+#    "(095) 234-5678\\n",
+#    "+380 44 123 4567",
+#    "380501234567",
+#    "    +38(050)123-32-34",
+#    "     0503451234",
+#    "(050)8889900",
+#    "38050-111-22-22",
+#    "38050 111 22 11   ",
+#]
